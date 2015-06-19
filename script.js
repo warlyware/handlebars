f31aaa9a0be388079586399b6d733d6f140e612c
warlyware <danward@gmail.com>
2015-06-19 15:24:05 -0700

    Add toggle for completed tasks, how many tasks left

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+var TODOS = [
+  { action: "Do this first", done: false },
+  { action: "Learn HB", done: true },
+  { action: "Learn Angular", done: false }
+];
+
 var source   = $("#entry-template").html();
 var template = Handlebars.compile(source);
 
-$.ajax('https://api.github.com/users').success(function(data) {
-  var html = template(data);
-  $("body").append(html);
+function render() {
+  var html    = template({ todos: TODOS, appName: 'Awesome' });
+  $(".container").html(html);
+}
+
+render();
+
+$('.container').on('click', ':checkbox', function() {
+  TODOS[$(this).data('index')].done = $(this).is(':checked');
+  render();
 });
