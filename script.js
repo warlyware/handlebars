67552b0317697014b822ad9196b1df8080a23de9
warlyware <danward@gmail.com>
2015-06-19 16:51:31 -0700

    Add color tags and clear all button

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 var TODOS = [
-  { action: "Do this first", done: false },
-  { action: "Learn HB", done: true },
-  { action: "Learn Angular", done: false }
+  { action: "Do this first", done: false, tag: 'important', cleared: false },
+  { action: "Learn HB", done: true, tag: 'semi-important', cleared: false },
+  { action: "Learn Angular", done: false, tag: 'unimportant', cleared: false }
 ];
 
 var source   = $("#entry-template").html();
 var template = Handlebars.compile(source);
 
 function render() {
-  var html    = template({ todos: TODOS, appName: 'Awesome' });
+  var html    = template({ todos: TODOS, appName: 'To Do App' });
   $(".container").html(html);
 }
 
 render();
 
-$('.container').on('click', ':checkbox', function() {
-  TODOS[$(this).data('index')].done = $(this).is(':checked');
+$('.container')
+  .on('click', ':checkbox', function() {
+    TODOS[$(this).data('index')].done = $(this).is(':checked');
+    render();
+  })
+  .on('click', '#clearDone', function() {
+    console.log('CLEAR');
+    TODOS = TODOS.filter(function(el) {
+      return !el.done;
+    });
+    render();
+  });
+
+$('#addTaskForm').on('submit', function(evt) {
+  evt.preventDefault();
+  console.log('submit');
+  var newTaskName = $('#newTaskName').val();
+  var newTaskTag = $('#newTaskTag option:selected').val();
+  TODOS.push({ action: newTaskName, done: false, tag: newTaskTag, cleared: false });
+  console.log(TODOS);
   render();
 });
